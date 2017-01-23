3e1c521376f04324bb2abb2bc1207b290f37eecf
mogedi <Mahadgd94@gmail.com>
2017-01-23 18:52:09 -0500

    First Commitment. Have Basic UI and some Code

---
+++ Alpha Health/CodeRunner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Alpha_Health
+{
+    class CodeRunner
+    {
+        public void populateComboBox(ComboBox comboBox)
+        {
+            comboBoxData data = new comboBoxData();
+
+            data.getComboBoxData();
+
+            foreach (string item in data.getComboBoxData())
+            {
+                comboBox.Items.Add(item);
+            }
+
+        }
+    }
+}
---
+++ Alpha Health/comboBoxData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alpha_Health
+{
+    class comboBoxData
+    {
+        private List<string> comboBoxList;
+
+        public comboBoxData()
+        {
+            comboBoxList = new List<string>(new string[] {"Enterprise", "Galactica", "HeartOfGold", "JupiterII",
+                "LobbyFX", "Normandy", "Nostromo", "Serenity", "Starfury", "Tigersclaw",
+                "WhiteStar", "Voyager", "Awing",  "Bwing", "Xwing", "Ywing"});
+        }
+
+        public List<string> getComboBoxData()
+        {
+            return comboBoxList;
+        }
+    }
+}
